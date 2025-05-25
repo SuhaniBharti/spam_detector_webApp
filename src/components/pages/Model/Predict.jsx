fef3f605997858a4e826c3c058edaa9fd86fb05c
Satish Singh <i.sksingh113@gmail.com>
2025-05-25 08:21:18 +0530

    4th commit

--- src/components/pages/Model/Predict.jsx
+++ src/components/pages/Model/Predict.jsx
@@ -10,7 +10,7 @@ const Predict = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://spam-sms-classifier-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: text }),
